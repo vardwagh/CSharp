0490939804a660617120d1c1c1f755520aa16e99
vardwagh <waghvardhan@gmail.com>
2022-07-08 20:14:26 +0530

    update

---
+++ CSharpPractice/Basic/Medium.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpPractice.Basic
+{
+    public class Medium
+    {
+
+        //Exercise-92 Next prime number of a given integer
+        public int test(int n)
+        {
+            for (int i = 2; i < n; i++)
+            {
+                if (n % i == 0) 
+                { 
+                    n++; 
+                    i = 2; 
+                }
+            }
+            return n;
+        }
+        //Exercise-91 Remove all the values except integer values from a given array of mixed values
+        public int[] test(object[] nums)
+        {
+            return nums.OfType<int>().ToArray();
+        }
+    }
+}
--- CSharpPractice/Program.cs
+++ CSharpPractice/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CSharpPractice.Basic;
 
 namespace CSharpPractice
 {
@@ -10,7 +11,20 @@ namespace CSharpPractice
     {
         private static void Main(string[] args)
         {
+            Medium m = new Medium();
 
+            int n = 120;
+            Console.WriteLine("Original number: " + n);
+            Console.WriteLine("Next prime number/Current prime number: " + m.test(n));
+            n = 321;
+            Console.WriteLine("\nOriginal number: " + n);
+            Console.WriteLine("Next prime number/Current prime number: " + m.test(n));
+            n = 43;
+            Console.WriteLine("\nOriginal number: " + n);
+            Console.WriteLine("Next prime number/Current prime number: " + m.test(n));
+            n = 4433;
+            Console.WriteLine("\nOriginal number: " + n);
+            Console.WriteLine("Next prime number/Current prime number: " + m.test(n)); ;
         }
     }
 }
